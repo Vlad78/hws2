cc9383fdc66131dbcce25a9f5a15f587823c824d
Vlad Razvenkov <dssdom@gmail.com>
2024-06-12 04:15:15 +0200

    HW10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,28 @@
 const initState = {
-    isLoading: false,
-}
+  isLoading: false,
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+type ActionType = ReturnType<typeof loadingAC>;
 
-        default:
-            return state
+export const loadingReducer = (state = initState, action: ActionType): typeof initState => {
+  // fix any
+  switch (action.type) {
+    case "CHANGE_LOADING": {
+      return { ...state, isLoading: action.isLoading };
     }
-}
+
+    default:
+      return state;
+  }
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+  type: "CHANGE_LOADING";
+  isLoading: boolean;
+};
 
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
+export const loadingAC = (isLoading: boolean): LoadingActionType =>
+  ({
+    type: "CHANGE_LOADING",
     isLoading,
-})
+  } as const);
